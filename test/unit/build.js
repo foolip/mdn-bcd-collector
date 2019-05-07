foolip/mdn-bcd-collector

commit f1db239573907a51e2c58ebb68f57ed8428dcac5

Update dependency webidl2 to v20 (#28)
* Update dependency webidl2 to v20 (renovate bot)
* Adapt to webidl2.js v20 (foolip)

build.js
@@ -219,16 +219,6 @@ function getExposureSet(node) {
 function buildIDLTests(ast) {
   const tests = [];
 
-  function nameOf(member) {
-    if (member.name) {
-      return member.name;
-    }
-    if (member.body && member.body.name) {
-      return member.body.name.value;
-    }
-    return undefined;
-  }
-
   const interfaces = ast.filter((dfn) => dfn.type === 'interface');
   interfaces.sort((a, b) => a.name.localeCompare(b.name));
 
@@ -254,35 +244,34 @@ function buildIDLTests(ast) {
     // members
     // TODO: iterable<>, maplike<>, setlike<> declarations are excluded
     // by filtering to things with names.
-    const members = iface.members.filter(nameOf);
-    members.sort((a, b) => nameOf(a).localeCompare(nameOf(b)));
+    const members = iface.members.filter((member) => member.name);
+    members.sort((a, b) => a.name.localeCompare(b.name));
 
     for (const member of members) {
       const isStatic = member.special && member.special.value === 'static';
-      const name = nameOf(member);
       let expr;
       switch (member.type) {
         case 'attribute':
         case 'operation':
           if (isGlobal) {
-            expr = `'${name}' in self`;
+            expr = `'${member.name}' in self`;
           } else if (isStatic) {
-            expr = `'${name}' in ${iface.name}`;
+            expr = `'${member.name}' in ${iface.name}`;
           } else {
-            expr = `'${name}' in ${iface.name}.prototype`;
+            expr = `'${member.name}' in ${iface.name}.prototype`;
           }
           break;
         case 'const':
           if (isGlobal) {
-            expr = `'${name}' in self`;
+            expr = `'${member.name}' in self`;
           } else {
-            expr = `'${name}' in ${iface.name}`;
+            expr = `'${member.name}' in ${iface.name}`;
           }
           break;
       }
 
       if (expr) {
-        tests.push([`${iface.name}.${name}`, expr]);
+        tests.push([`${iface.name}.${member.name}`, expr]);
       } else {
         // eslint-disable-next-line max-len
         console.warn(`Interface ${iface.name} member type ${member.type} not handled`);
@@ -304,13 +293,12 @@ function buildIDLTests(ast) {
     tests.push([namespace.name, `'${namespace.name}' in self`]);
 
     // members
-    const members = namespace.members.filter(nameOf);
-    members.sort((a, b) => nameOf(a).localeCompare(nameOf(b)));
+    const members = namespace.members.filter((member) => member.name);
+    members.sort((a, b) => a.name.localeCompare(b.name));
 
     for (const member of members) {
-      const name = nameOf(member);
-      tests.push([`${namespace.name}.${name}`,
-        `'${name}' in ${namespace.name}`]);
+      tests.push([`${namespace.name}.${member.name}`,
+        `'${member.name}' in ${namespace.name}`]);
     }
   }
 

test/unit/build.js
@@ -50,7 +50,7 @@ describe('build', () => {
       assert.lengthOf(namespace.members, 2);
       assert.containSubset(namespace.members[0], {
         type: 'operation',
-        body: {name: {value: 'supports'}},
+        name: 'supports',
       });
       assert.containSubset(namespace.members[1], {
         type: 'attribute',

commit a6f540b04153ea7cc43400d8cf4d6d7e29d83468

Fix and test IDL static operations after webidl2.js upgrade
Broken by https://github.com/foolip/mdn-bcd-collector/pull/28 and not
noticed because there were no tests.

build.js
@@ -248,7 +248,7 @@ function buildIDLTests(ast) {
     members.sort((a, b) => a.name.localeCompare(b.name));
 
     for (const member of members) {
-      const isStatic = member.special && member.special.value === 'static';
+      const isStatic = member.special === 'static';
       let expr;
       switch (member.type) {
         case 'attribute':

test/unit/build.js
@@ -79,6 +79,17 @@ describe('build', () => {
       ]);
     });
 
+    it('interface with static method', () => {
+      const ast = WebIDL2.parse(
+          `interface MediaSource {
+             static boolean isTypeSupported(DOMString type);
+           };`);
+      assert.deepEqual(buildIDLTests(ast), [
+        ['MediaSource', '\'MediaSource\' in self'],
+        ['MediaSource.isTypeSupported', '\'isTypeSupported\' in MediaSource'],
+      ]);
+    });
+
     it('namespace with attribute', () => {
       const ast = WebIDL2.parse(
           `namespace CSS {