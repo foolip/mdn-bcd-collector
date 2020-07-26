63ab9ceddfaa83e9bcd93bde2564bf4b20c3470a
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2020-07-26 03:33:32 -0700

    Add TODO for prefixed support tests

--- build.js
+++ build.js
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// TODO: Test for support under a prefix
+
 'use strict';
 
 const fs = require('fs-extra');
