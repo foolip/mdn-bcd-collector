00d9665715c42f69412cefa401ad0b6da985f0b8
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2020-08-08 19:45:19 -0700

    Make Selenium testing more test environment friendly

--- test/selenium/selenium.js
+++ test/selenium/selenium.js
@@ -1,12 +1,7 @@
 const {Builder, By, until} = require('selenium-webdriver');
 const bcd = require('mdn-browser-compat-data');
 
-const secrets = require('../../secrets.json');
-
-const host = process.env.NODE_ENV === 'test' ?
-      `http://localhost:8080` :
-      'http://mdn-bcd-collector.appspot.com';
-
+// TODO: define target browsers
 const browsersToTest = {
   'chrome': Object.keys(bcd.browsers.chrome.releases).filter((k) => (k >= 26)),
   'edge': Object.keys(bcd.browsers.edge.releases).filter((k) => (k >= 13)),
@@ -14,6 +9,22 @@ const browsersToTest = {
   'ie': Object.keys(bcd.browsers.ie.releases).filter((k) => (k >= 9)),
   'safari': Object.keys(bcd.browsers.safari.releases).filter((k) => (k >= 8))
 };
+
+const secrets = require('../../secrets.json');
+
+const host = process.env.NODE_ENV === 'test' ?
+      `http://localhost:8080` :
+      'http://mdn-bcd-collector.appspot.com';
+
+const seleniumUrl = secrets.selenium.url && secrets.selenium.url
+    .replace('$USERNAME$', secrets.selenium.username)
+    .replace('$ACCESSKEY$', secrets.selenium.accesskey);
+
+if (!seleniumUrl) {
+  // eslint-disable-next-line max-len
+  console.error('A Selenium remote WebDriver URL is not defined in secrets.json.  Please define your Selenium remote.');
+  process.exit(1);
+}
 
 // eslint-disable-next-line guard-for-in
 for (const browser in browsersToTest) {
@@ -24,9 +35,8 @@ for (const browser in browsersToTest) {
       let driver;
 
       beforeEach(function() {
-        driver = new Builder().usingServer(
-            `https://${secrets.saucelabs.username}:${secrets.saucelabs.access_key}@ondemand.us-west-1.saucelabs.com:443/wd/hub`
-        ).forBrowser(browser, version).build();
+        driver = new Builder().usingServer(seleniumUrl)
+            .forBrowser(browser, version).build();
       });
 
       afterEach(async function() {
