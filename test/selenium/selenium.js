6c2a928cce268b6aec7c6695ee7250b82ba6e245
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2020-08-08 21:57:17 -0700

    Add Selenium WebDriver support (#346)
    * Use ID for test begin button
    
    * Add selenium-webdriver
    
    * Add SauceLabs secrets
    
    * Add Selenium runtime support
    
    * Fix browser iteration
    
    * Collect release data from BCD
    
    * Add TODO comment
    
    * Only keep version numbers for releases
    
    * Limit browser versions to test
    
    * Exclude Selenium when running test
    
    * Fix linting issues
    
    * Don't filter out Safari's minor releases
    
    * Remove outdated Todo
    
    * Make Selenium testing more test environment friendly
    
    * Add Selenium info to WebDriver
    
    * Save Selenium as dev dependency instead
    
    * Use capabilities instead
    
    * Downgrade to Selenium v3.6
    
    * Fix linting issues

---
+++ test/selenium/selenium.js
@@ -0,0 +1,79 @@
+const {
+  Browser,
+  Builder,
+  By,
+  Capabilities,
+  Capability,
+  until
+} = require('selenium-webdriver');
+const bcd = require('mdn-browser-compat-data');
+
+// TODO: define target browsers
+const browsersToTest = {
+  'chrome': Object.keys(bcd.browsers.chrome.releases).filter((k) => (k >= 26)),
+  'edge': Object.keys(bcd.browsers.edge.releases).filter((k) => (k >= 13)),
+  'firefox': Object.keys(bcd.browsers.firefox.releases).filter((k) => (k >= 4)),
+  'ie': Object.keys(bcd.browsers.ie.releases).filter((k) => (k >= 9)),
+  'safari': Object.keys(bcd.browsers.safari.releases).filter((k) => (k >= 8))
+};
+
+const secrets = require('../../secrets.json');
+
+const host = process.env.NODE_ENV === 'test' ?
+      `http://localhost:8080` :
+      'http://mdn-bcd-collector.appspot.com';
+
+const seleniumUrl = secrets.selenium.url && secrets.selenium.url
+    .replace('$USERNAME$', secrets.selenium.username)
+    .replace('$ACCESSKEY$', secrets.selenium.accesskey);
+
+if (!seleniumUrl) {
+  // eslint-disable-next-line max-len
+  console.error('A Selenium remote WebDriver URL is not defined in secrets.json.  Please define your Selenium remote.');
+  process.exit(1);
+}
+
+// eslint-disable-next-line guard-for-in
+for (const browser in browsersToTest) {
+  for (const version of browsersToTest[browser]) {
+    describe(`${bcd.browsers[browser].name} ${version}`, function() {
+      this.timeout(30000);
+      this.slow(15000);
+      let driver;
+
+      beforeEach(function() {
+        const capabilities = new Capabilities();
+        capabilities.set(
+            Capability.BROWSER_NAME,
+            Browser[browser.toUpperCase()]
+        );
+        capabilities.set(Capability.VERSION, version);
+
+        driver = new Builder().usingServer(seleniumUrl)
+            .withCapabilities(capabilities).build();
+      });
+
+      afterEach(async function() {
+        await driver.quit();
+      });
+
+      it('run', async function() {
+        await driver.get(host);
+        await driver.wait(
+            until.elementIsEnabled(
+                await driver.findElement(By.id('start')), 'Run'
+            ),
+            30000
+        );
+        await driver.findElement(By.id('start')).click();
+        await driver.wait(until.urlIs(`${host}/results/`), 30000);
+        await driver.wait(
+            until.elementTextContains(
+                await driver.findElement(By.id('status')), 'to'
+            ),
+            30000
+        );
+      });
+    });
+  }
+}
