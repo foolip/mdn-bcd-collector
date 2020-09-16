fce4a23a0f341066608cb3f5a1aca4da3ce4e83d
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2020-09-16 12:40:57 -0700

    Fix ESLint issues and configuration

--- static/resources/custom-tests/api/AudioWorkletNode/WhiteNoiseProcessor.js
+++ static/resources/custom-tests/api/AudioWorkletNode/WhiteNoiseProcessor.js
@@ -1,15 +1,17 @@
 // Custom test worklet for api.AudioWorkletNode, example copied from:
 // https://developer.mozilla.org/en-US/docs/Web/API/AudioWorkletNode
 
+/* global AudioWorkletProcessor, registerProcessor */
+
 class WhiteNoiseProcessor extends AudioWorkletProcessor {
-  process (inputs, outputs, parameters) {
-    const output = outputs[0]
-    output.forEach(channel => {
+  process(inputs, outputs, parameters) {
+    const output = outputs[0];
+    output.forEach((channel) => {
       for (let i = 0; i < channel.length; i++) {
-        channel[i] = Math.random() * 2 - 1
+        channel[i] = Math.random() * 2 - 1;
       }
-    })
-    return true
+    });
+    return true;
   }
 }
 
