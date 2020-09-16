67ba755f365e8b32ae3b9f392ad96e22989fcaaa
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2020-09-16 11:26:55 -0700

    Add custom test for AudioWorkletNode and AudioParamMap (broken)

---
+++ static/resources/custom-tests/api/AudioWorkletNode/WhiteNoiseProcessor.js
@@ -0,0 +1,16 @@
+// Custom test worklet for api.AudioWorkletNode, example copied from:
+// https://developer.mozilla.org/en-US/docs/Web/API/AudioWorkletNode
+
+class WhiteNoiseProcessor extends AudioWorkletProcessor {
+  process (inputs, outputs, parameters) {
+    const output = outputs[0]
+    output.forEach(channel => {
+      for (let i = 0; i < channel.length; i++) {
+        channel[i] = Math.random() * 2 - 1
+      }
+    })
+    return true
+  }
+}
+
+registerProcessor('white-noise-processor', WhiteNoiseProcessor);
