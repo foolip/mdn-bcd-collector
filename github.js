e52fa38f5970a9546d549ef035e7a0eef3bed9b9
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2020-11-13 06:21:28 -0800

    Fix linting

--- github.js
+++ github.js
@@ -38,7 +38,7 @@ const github = (options) => {
     const os = `${ua.os.name} ${ua.os.version}`;
     const desc = `${browser} / ${os}`;
     const title = `${dev ? '(Dev) ' : ''}Results from ${desc} / Collector v${version}`;
-    
+
     const slug = `${version}-${slugify(desc, {lower: true})}-${digest}`;
     const filename = `${slug}.json`;
     const branch = `collector/${slug}`;
