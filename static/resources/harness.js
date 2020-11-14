006bacc619e2f68f8386954440e689e402139a6a
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2020-11-13 22:22:14 -0800

    Run tests one by one to prevent too many promises at once

--- static/resources/harness.js
+++ static/resources/harness.js
@@ -227,14 +227,15 @@
     var oncomplete = function(result) {
       results.push(result);
       completedTests += 1;
+
       if (completedTests >= tests.length) {
         callback(results);
+      } else {
+        runTest(tests[completedTests], 0, oncomplete);
       }
     };
 
-    for (var i = 0; i < tests.length; i++) {
-      runTest(tests[i], 0, oncomplete);
-    }
+    runTest(tests[0], 0, oncomplete);
   }
 
   function runWindow(callback, results) {
