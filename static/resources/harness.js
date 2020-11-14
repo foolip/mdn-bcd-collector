3221b66ad3cfb1f2bb4dd8826c65c8ff2652a14c
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2020-11-14 15:56:57 -0800

    Increase timeout before showing possible failure message

--- static/resources/harness.js
+++ static/resources/harness.js
@@ -401,7 +401,7 @@
       var timeout = setTimeout(function() {
         updateStatus('<br />This test seems to be taking a long time; ' +
             'it may have crashed. Check the console for errors.', true);
-      }, 20000);
+      }, 30000);
 
       runWindow(function(results) {
         runWorker(function(results) {
