3d77375d80db5b5a600f73eb871f0d8de0723dfe
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2020-11-17 01:34:59 -0800

    More lenient minor version matching at end

--- ua-parser.js
+++ ua-parser.js
@@ -51,22 +51,21 @@ const parseUA = (userAgent, browsers) => {
   // with this, find the pair of versions in |versions| that sandwiches
   // |version|, and use the first of this pair. For example, given |version|
   // "10.1" and |versions| entries "10.0" and "10.2", return "10.0".
-  for (let i = 0; i < versions.length; i++) {
+  for (let i = 0; i < versions.length - 1; i++) {
     const current = versions[i];
     const next = versions[i + 1];
-    if (next) {
-      if (compareVersions.compare(version, current, '>=') &&
-          compareVersions.compare(version, next, '<')) {
-        return {browser, version: current, inBcd: true};
-      }
-    } else {
-      // This is the last entry in |versions|. With no |next| to compare against
-      // we have to match the version more conservatively, requiring major and
-      // minor versions to match. "10.0" and "10" are seen as equivalent.
-      if (getMajorMinorVersion(version) === getMajorMinorVersion(current)) {
-        return {browser, version: current, inBcd: true};
-      }
+    if (compareVersions.compare(version, current, '>=') &&
+        compareVersions.compare(version, next, '<')) {
+      return {browser, version: current, inBcd: true};
     }
+  }
+
+  // This is the last entry in |versions|. With no |next| to compare against
+  // we have to check that the major versions match. Given |version| "10.3"
+  // and |versions| entries "10.0" and "10.2", return "10.2". Given |version|
+  // "11.0", skip.
+  if (version.split('.')[0] === versions[versions.length-1].split('.')[0]) {
+    return {browser, version: current, inBcd: true};
   }
 
   return {browser, version, inBcd: false};
