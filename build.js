c6af73d720c599b756ab0682b6d30666391a4e89
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2020-11-23 21:07:23 -0800

    Map 'DedicatedWorker' as 'Worker'

--- build.js
+++ build.js
@@ -406,6 +406,12 @@ const getExposureSet = (node) => {
     default:
       throw new Error(`Unexpected RHS for Exposed extended attribute`);
   }
+
+  if (globals.has('DedicatedWorker')) {
+    globals.delete('DedicatedWorker');
+    globals.add('Worker');
+  }
+
   return globals;
 };
 
