49b465db50b1e3399771e8cf646f4ad72c43247e
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2020-11-24 03:50:22 -0800

    Fix Safari minimum version for Selenium

--- selenium.js
+++ selenium.js
@@ -319,7 +319,7 @@ const runAll = async (limitBrowsers, oses, nonConcurrent) => {
     edge: filterVersions(bcd.browsers.edge.releases, 12),
     firefox: filterVersions(bcd.browsers.firefox.releases, 4),
     ie: filterVersions(bcd.browsers.ie.releases, 6),
-    safari: filterVersions(bcd.browsers.safari.releases, 4)
+    safari: filterVersions(bcd.browsers.safari.releases, 5.1)
   };
 
   if (limitBrowsers) {
