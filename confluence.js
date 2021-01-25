6e27849366218cd882b0e8ebd8e5ba014b5a5a04
Philip Jägenstedt <philip@foolip.org>
2021-01-25 16:35:30 +0100

    filename from argv

--- confluence.js
+++ confluence.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
 const tests = require('./tests.json');
 
-const lines = fs.readFileSync('../confluence.md/confluence.md', 'utf8')
+const filename = process.argv[2];
+const lines = fs.readFileSync(filename, 'utf8')
                   .split('\n').filter(l => l);
 
 function capitalize(str) {
     return str[0].toUpperCase() + str.substr(1);
 }
 
-for (const line of lines) {
+const outLines = lines.map(line => {
     const [iface, member] = line.substr(6).split(' ')[0].split('#');
     let haveTest = false;
     if (`api.${iface}.${member}` in tests) {
@@ -19,5 +20,8 @@ for (const line of lines) {
         haveTest = true;
     }
     const check = line.startsWith('- [x] ') || haveTest ? 'x' : ' ';
-    console.log(`- [${check}] ${line.substr(6)}`);
-}
+    return `- [${check}] ${line.substr(6)}`;
+});
+outLines.push('');
+
+fs.writeFileSync(filename, outLines.join('\n'), 'utf8');
