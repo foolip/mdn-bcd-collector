cd31c3a92e296332bd654dce246fbd6654edbd31
Philip Jägenstedt <philip@foolip.org>
2021-02-23 20:33:52 +0100

    Update selenium.js

--- selenium.js
+++ selenium.js
@@ -256,7 +256,7 @@ const run = async (browser, version, os, ctx, task) => {
     await driver.wait(until.elementLocated(By.id('status')), 30000);
     statusEl = await driver.findElement(By.id('status'));
     try {
-      await driver.wait(until.elementTextContains(statusEl, 'uploaded'), 30000);
+      await driver.wait(until.elementTextContains(statusEl, 'uploaded'), 45000);
     } catch (e) {
       if (e.name == 'TimeoutError') {
         throw new Error('Timed out waiting for results to upload');
