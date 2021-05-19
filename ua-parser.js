d78ceace6fe940e409aaba5012815a430d9c7fba
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2021-05-19 01:00:02 -0700

    Fix formatting

--- ua-parser.js
+++ ua-parser.js
@@ -49,14 +49,14 @@ const parseUA = (userAgent, browsers) => {
   }
 
   data.fullVersion = ua.browser.version;
-  
+
   if (data.browser.id === 'safari_ios') {
     // https://github.com/mdn/browser-compat-data/blob/main/docs/data-guidelines.md#safari-for-ios-versioning
     data.fullVersion = ua.os.version;
   } else if (ua.browser.name === 'Android Browser') {
-    data.fullVersion = compareVersions.compare(ua.os.version, '5.0', '<')
-      ? ua.os.version
-      : ua.engine.version;
+    data.fullVersion = compareVersions.compare(ua.os.version, '5.0', '<') ?
+      ua.os.version :
+      ua.engine.version;
   }
 
   data.version = getMajorMinorVersion(data.fullVersion);
