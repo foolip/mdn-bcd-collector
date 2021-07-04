bb3bb3e320094e0435c461469dfc5d790cb27271
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2021-07-04 13:24:52 -0700

    Catch results upload failures

--- selenium.js
+++ selenium.js
@@ -301,13 +301,19 @@ const run = async (browser, version, os, ctx, task) => {
     await driver.wait(until.elementLocated(By.id('status')), 30000);
     statusEl = await driver.findElement(By.id('status'));
     try {
-      await driver.wait(until.elementTextContains(statusEl, 'uploaded'), 60000);
+      await driver.wait(until.elementTextContains(statusEl, 'upload'), 60000);
     } catch (e) {
       if (e.name == 'TimeoutError') {
         throw new Error(task.title + ' - ' + 'Timed out waiting for results to upload');
       }
 
       throw e;
+    }
+
+    const statusText = await statusEl.getText();
+
+    if (statusText.includes('Failed')) {
+      throw new Error(task.title + ' - ' + statusText);
     }
 
     log(task, 'Exporting results...');
