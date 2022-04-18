6a90eff8d30f7ea0cb52a9592da2caf9906721e0
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-04-18 02:40:48 -0700

    Set to production by default if NODE_ENV = production (#1903)
    * Set to production by default if NODE_ENV = production
    
    * Remove GAE_VERSION logic entirely

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const storage = getStorage();
 
 /* c8 ignore start */
 const appVersion =
-  process.env.GAE_VERSION === 'production' ?
+  process.env.NODE_ENV === 'production' ?
     (await fs.readJson(new URL('./package.json', import.meta.url))).version :
     'Dev';
 
