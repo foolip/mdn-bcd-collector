a72e54e404e90f0ddec296c5de1d8612ccd33814
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-04-18 06:41:04 -0700

    Prefer HTTPS

--- selenium.js
+++ selenium.js
@@ -49,7 +49,7 @@ const seleniumUrls = {
   browserstack: 'https://${username}:${key}@hub-cloud.browserstack.com/wd/hub',
   saucelabs:
     'https://${username}:${key}@ondemand.${region}.saucelabs.com:443/wd/hub',
-  lambdatest: 'http://${username}:${key}@hub.lambdatest.com/wd/hub'
+  lambdatest: 'https://${username}:${key}@hub.lambdatest.com/wd/hub'
 };
 
 // Custom tests that use getUserMedia() make Edge 12-18 and Firefox 34-53 block.
