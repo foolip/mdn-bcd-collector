bc4900c0581d3a2f9064bccbfa129dca8b1a9c78
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-06-21 16:42:34 -0700

    Fix formatting

--- selenium-keepalive.js
+++ selenium-keepalive.js
@@ -9,35 +9,34 @@
 import http from 'http';
 import https from 'https';
 
-//set the time (in seconds) for connection to be alive
-var keepAliveTimeout = 30 * 1000;
+// set the time (in seconds) for connection to be alive
+const keepAliveTimeout = 30 * 1000;
 
-if (http.globalAgent && http.globalAgent.hasOwnProperty('keepAlive')) {
+if (http.globalAgent && 'keepAlive' in http.globalAgent) {
   http.globalAgent.keepAlive = true;
   https.globalAgent.keepAlive = true;
   http.globalAgent.keepAliveMsecs = keepAliveTimeout;
   https.globalAgent.keepAliveMsecs = keepAliveTimeout;
 } else {
-  var agent = new http.Agent({
+  const agent = new http.Agent({
     keepAlive: true,
     keepAliveMsecs: keepAliveTimeout
   });
 
-  var secureAgent = new https.Agent({
+  const secureAgent = new https.Agent({
     keepAlive: true,
     keepAliveMsecs: keepAliveTimeout
   });
 
-  var httpRequest = http.request;
-  var httpsRequest = https.request;
+  const httpRequest = http.request;
+  const httpsRequest = https.request;
 
-  http.request = function (options, callback) {
+  http.request = (options, callback) => {
     if (options.protocol == 'https:') {
       options['agent'] = secureAgent;
       return httpsRequest(options, callback);
-    } else {
-      options['agent'] = agent;
-      return httpRequest(options, callback);
     }
+    options['agent'] = agent;
+    return httpRequest(options, callback);
   };
 }
