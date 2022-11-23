1a8f008291ff7d6035ef6746206b5139b7a58631
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-11-23 03:36:13 -0800

    Replace scripts.ts with prepare.ts; move exec function to its own file
    Cherry-picked from https://github.com/GooborgStudios/mdn-bcd-collector/commit/1fbb63ff42e72f001d0cb9fe7e49523fbe5e7e06.

---
+++ lib/exec.ts
@@ -0,0 +1,23 @@
+//
+// mdn-bcd-collector: lib/exec.ts
+// Execute a command
+//
+// Gooborg Studios
+// See the LICENSE file for copyright details
+//
+
+import childProcess from 'child_process';
+
+const exec = async (cmd, env?: any, pipe = true) => {
+  env = {...process.env, ...env};
+  if (!pipe) {
+    console.log(`> ${cmd}`);
+  }
+  const output = await childProcess.execSync(cmd, {
+    env,
+    stdio: pipe ? 'pipe' : 'inherit'
+  });
+  return output.toString();
+};
+
+export default exec;
