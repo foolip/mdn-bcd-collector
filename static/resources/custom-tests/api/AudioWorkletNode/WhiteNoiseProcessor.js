c9a24411b9471588d0e12591b56a2a34bb5a7185
Philip Jägenstedt <philip@foolip.org>
2023-01-03 17:29:29 +0700

    Remove unused parameter

--- static/resources/custom-tests/api/AudioWorkletNode/WhiteNoiseProcessor.js
+++ static/resources/custom-tests/api/AudioWorkletNode/WhiteNoiseProcessor.js
@@ -10,7 +10,7 @@
 /* global AudioWorkletProcessor, registerProcessor */
 
 class WhiteNoiseProcessor extends AudioWorkletProcessor {
-  process(inputs, outputs, parameters) {
+  process(inputs, outputs) {
     const output = outputs[0];
     output.forEach((channel) => {
       for (let i = 0; i < channel.length; i++) {
